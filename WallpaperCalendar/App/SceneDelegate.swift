6405a3c1131407298617141ac39095ab3bcc2cdd
Pavel Grigorev <amorfien@gmail.com>
2026-02-05 17:17:21 +0300

    First Commit

---
+++ WallpaperCalendar/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  WallpaperCalendar
+//
+//  Created by Pavel Grigorev on 05.02.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.overrideUserInterfaceStyle = .light
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
