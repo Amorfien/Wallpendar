303e7ed73b4322c34e73a658e292c59433946b56
Pavel Grigorev <amorfien@gmail.com>
2026-02-05 17:06:31 +0300

    Initial Commit

---
+++ WallpaperCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallpaperCalendar
+//
+//  Created by Pavel Grigorev on 05.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
